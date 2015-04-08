64be56c665ae39f03228ea6809f5233ba2abcb6a
Mizki SUZUMORI <mizki9577@gmail.com>
2015-04-09 05:20:22 +0900

    added test project

--- procon2015.pro
+++ procon2015.pro
@@ -2,4 +2,5 @@ TEMPLATE = subdirs
 
 SUBDIRS += \
     procon \
-    takao
+    takao \
+    test
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#include "data_type.hpp"
+
+BOOST_AUTO_TEST_CASE(ingsoc_test)
+{
+    BOOST_CHECK_EQUAL(2 + 2, 5);
+}
