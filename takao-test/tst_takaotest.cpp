9c2e2a3cd7052da751cd161b523d6740c9e5d20d
Mizki SUZUMORI <mizki9577@gmail.com>
2015-04-20 18:36:12 +0900

    _split のテスト追加ついでに色々

--- takao-test/tst_takaotest.cpp
+++ takao-test/tst_takaotest.cpp
@@ -1,6 +1,10 @@
 #include <QString>
 #include <QtTest>
 
+#define private public
+#include "data_type.hpp"
+#undef private
+
 class TakaoTest : public QObject
 {
         Q_OBJECT
@@ -9,16 +13,20 @@ class TakaoTest : public QObject
         TakaoTest();
 
     private Q_SLOTS:
-        void testCase1();
+        void _split_test();
 };
 
 TakaoTest::TakaoTest()
 {
 }
 
-void TakaoTest::testCase1()
+void TakaoTest::_split_test()
 {
-    QVERIFY2(true, "Failure");
+    using namespace std::string_literals;
+    auto sample = "123,,456,,789,,12,34,56,78,90,,abc,,def,,ghi,,,,,"s;
+    std::vector<std::string> result = _split(sample, ",,"s);
+    std::vector<std::string> expected = {"123"s,"456"s,"789"s, "12,34,56,78,90"s, "abc"s, "def"s, "ghi"s, ""s, ","s};
+    QCOMPARE(expected, result);
 }
 
 QTEST_APPLESS_MAIN(TakaoTest)
