b886756ab8a5690bb065fa36d60626d34321683a
Mizki SUZUMORI <mizki9577@gmail.com>
2015-04-22 01:51:28 +0900

    stone_type のコンストラクタのテストを書いた

--- test/stone_type_test/stone_type_test.cpp
+++ test/stone_type_test/stone_type_test.cpp
@@ -3,6 +3,8 @@
 #define private public
 #include "data_type.hpp"
 #undef private
+Q_DECLARE_METATYPE(std::string)
+Q_DECLARE_METATYPE(stone_type::raw_stone_type)
 
 class stone_type_test : public QObject
 {
@@ -12,16 +14,100 @@ class stone_type_test : public QObject
         stone_type_test();
 
     private Q_SLOTS:
-        void test_case_1();
+        void construct_test_data();
+        void construct_test();
 };
 
 stone_type_test::stone_type_test()
 {
 }
 
-void stone_type_test::test_case_1()
+void stone_type_test::construct_test_data()
 {
-    QVERIFY2(true, "Failure");
+    using namespace std::string_literals;
+    QTest::addColumn<std::string>("raw_stone");
+    QTest::addColumn<stone_type::raw_stone_type>("result");
+
+    QTest::newRow("1")
+        << "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01110000\r\n"
+           "00000000"s
+        << stone_type::raw_stone_type{{
+           {{0, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 1, 1, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}}}};
+    QTest::newRow("2")
+        << "00000000\r\n"
+           "01100000\r\n"
+           "01100000\r\n"
+           "01100000\r\n"
+           "01100000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000"s
+        << stone_type::raw_stone_type{{
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 1, 0, 0, 0, 0, 0}},
+           {{0, 1, 1, 0, 0, 0, 0, 0}},
+           {{0, 1, 1, 0, 0, 0, 0, 0}},
+           {{0, 1, 1, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}}}};
+    QTest::newRow("3")
+        << "00000000\r\n"
+           "00010000\r\n"
+           "00010000\r\n"
+           "01111000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000"s
+        << stone_type::raw_stone_type{{
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 1, 0, 0, 0, 0}},
+           {{0, 0, 0, 1, 0, 0, 0, 0}},
+           {{0, 1, 1, 1, 1, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}},
+           {{0, 0, 0, 0, 0, 0, 0, 0}}}};
+    QTest::newRow("4")
+        << "10000000\r\n"
+           "11000000\r\n"
+           "01100000\r\n"
+           "00110000\r\n"
+           "00011000\r\n"
+           "00001100\r\n"
+           "00000110\r\n"
+           "00000011"s
+        << stone_type::raw_stone_type{{
+           {{1, 0, 0, 0, 0, 0, 0, 0}},
+           {{1, 1, 0, 0, 0, 0, 0, 0}},
+           {{0, 1, 1, 0, 0, 0, 0, 0}},
+           {{0, 0, 1, 1, 0, 0, 0, 0}},
+           {{0, 0, 0, 1, 1, 0, 0, 0}},
+           {{0, 0, 0, 0, 1, 1, 0, 0}},
+           {{0, 0, 0, 0, 0, 1, 1, 0}},
+           {{0, 0, 0, 0, 0, 0, 1, 1}}}};
+}
+
+void stone_type_test::construct_test()
+{
+    QFETCH(std::string, raw_stone);
+    QFETCH(stone_type::raw_stone_type, result);
+
+    QCOMPARE(stone_type(raw_stone, 0).raw_data, result);
 }
 
 QTEST_APPLESS_MAIN(stone_type_test)
