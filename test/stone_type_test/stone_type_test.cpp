10f0e8301a809392f7b93130590c266c939e4775
Mizki SUZUMORI <mizki9577@gmail.com>
2015-04-23 00:34:02 +0900

    rotate() のテストを追加

--- test/stone_type_test/stone_type_test.cpp
+++ test/stone_type_test/stone_type_test.cpp
@@ -4,6 +4,7 @@
 #include "data_type.hpp"
 #undef private
 Q_DECLARE_METATYPE(std::string)
+Q_DECLARE_METATYPE(stone_type)
 Q_DECLARE_METATYPE(stone_type::raw_stone_type)
 
 class stone_type_test : public QObject
@@ -16,6 +17,9 @@ class stone_type_test : public QObject
     private Q_SLOTS:
         void construct_test_data();
         void construct_test();
+
+        void rotate_test_data();
+        void rotate_test();
 };
 
 stone_type_test::stone_type_test()
@@ -54,6 +58,72 @@ void stone_type_test::construct_test()
     QFETCH(stone_type::raw_stone_type, raw_stone);
 
     QCOMPARE(stone_type(stone_text, 0).get_array(), raw_stone);
+}
+
+void stone_type_test::rotate_test_data()
+{
+    using namespace std::string_literals;
+    QTest::addColumn<stone_type>("stone");
+    QTest::addColumn<stone_type>("rotated90");
+    QTest::addColumn<stone_type>("rotated180");
+    QTest::addColumn<stone_type>("rotated270");
+
+    QTest::newRow("1")
+        << stone_type(
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "01110000\r\n"
+           "00000000"s, 0)
+        << stone_type(
+           "00000000\r\n"
+           "01111111\r\n"
+           "01000000\r\n"
+           "01000000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000"s, 0)
+        << stone_type(
+           "00000000\r\n"
+           "00001110\r\n"
+           "00000010\r\n"
+           "00000010\r\n"
+           "00000010\r\n"
+           "00000010\r\n"
+           "00000010\r\n"
+           "00000010"s, 0)
+        << stone_type(
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000000\r\n"
+           "00000010\r\n"
+           "00000010\r\n"
+           "11111110\r\n"
+           "00000000"s, 0);
+}
+
+void stone_type_test::rotate_test()
+{
+    QFETCH(stone_type, stone);
+    QFETCH(stone_type, rotated90);
+    QFETCH(stone_type, rotated180);
+    QFETCH(stone_type, rotated270);
+
+    QCOMPARE(stone.rotate(90), rotated90);
+    QCOMPARE(stone.rotate(180), rotated180);
+    QCOMPARE(stone.rotate(270), rotated270);
+    QCOMPARE(stone.rotate(360), stone);
+    QCOMPARE(stone.rotate(450), rotated90);
+    QCOMPARE(stone.rotate(-90), rotated270);
+    QCOMPARE(stone.rotate(-180), rotated180);
+    QCOMPARE(stone.rotate(-270), rotated90);
+    QCOMPARE(stone.rotate(-360), stone);
+    QCOMPARE(stone.rotate(-450), rotated270);
 }
 
 QTEST_APPLESS_MAIN(stone_type_test)
