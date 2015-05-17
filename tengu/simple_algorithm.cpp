50284537ac5f620404b59812a25c87a9eae2b867
kinironote <kinironote@gmail.com>
2015-05-17 22:21:17 +0900

    [ちょっとfix]サンプルアルゴリズムの、端っこに石が置かれないバグを修正

--- tengu/simple_algorithm.cpp
+++ tengu/simple_algorithm.cpp
@@ -19,8 +19,8 @@ field_type simple_algorithm::run(std::string problem_data){
 
     //端から置ける場所を探して石を挿入していく
     for(unsigned int stone_num=0;stone_num<problem.stones.size();stone_num++){
-        for(int dy=0;dy<=32;dy++){
-            for(int dx=0;dx<=32;dx++){
+        for(int dy=-8;dy<=32;dy++){
+            for(int dx=-8;dx<=32;dx++){
                 //フィールドに置けるかチェック
                 if(problem.field.is_puttable(problem.stones[stone_num],dy,dx)){
                     //設置
