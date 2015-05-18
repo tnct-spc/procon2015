b1213d35789496a740f4c5ef21f78f63263ace3f
imoneg <kazu.takishima@gmail.com>
2015-05-19 01:17:40 +0900

    lambda式をループの中に直接書いた. 謎の標準出力を消した

--- tengu/algorithm/simple_algorithm.cpp
+++ tengu/algorithm/simple_algorithm.cpp
@@ -13,49 +13,23 @@ simple_algorithm::~simple_algorithm()
 }
 
 void simple_algorithm::run(){
-
-    //problem_type problem(problem_data);
-
-    auto put_func = [&](stone_type stone){
-        for(int dy=-8;dy<=32;dy++){
-            for(int dx=-8;dx<=32;dx++){
-                for(int flip = 0; flip < 2; flip ++){
-                     for(int angle = 0; angle < 4; angle ++){
-                         if(problem.field.is_puttable(stone,dy,dx)){
-                             problem.field.put_stone(stone,dy,dx);
-                             return;
-                         }
-                         stone.rotate(90);
-                      }
-                  stone.flip();
+    for(auto stone : problem.stones){
+        [&]{
+            for(int dy=-8;dy<=32;dy++){
+                for(int dx=-8;dx<=32;dx++){
+                    for(int flip = 0; flip < 2; flip ++){
+                        for(int angle = 0; angle < 4; angle ++){
+                            if(problem.field.is_puttable(stone,dy,dx)){
+                                problem.field.put_stone(stone,dy,dx);
+                                return;
+                            }
+                            stone.rotate(90);
+                        }
+                        stone.flip();
+                    }
                 }
             }
-        }
-    };
-    /*
-    //端から置ける場所を探して石を挿入していく
-    for(unsigned int stone_num=0;stone_num<problem.stones.size();stone_num++){
-        for(int dy=-8;dy<=32;dy++){
-            for(int dx=-8;dx<=32;dx++){
-                //フィールドに置けるかチェック
-                if(problem.field.is_puttable(problem.stones[stone_num],dy,dx)){
-                    //設置
-                    problem.field.put_stone(problem.stones[stone_num],dy,dx);
-                    goto NEXT_STONE;
-                    //お手軽GOTOを使うな
-                }
-            }
-        }
-        NEXT_STONE:;
+        }();
     }
-    */
-    for(auto _stone : problem.stones){
-                put_func(_stone);
-    }
-
-    for(auto stone : problem.field.list_of_stones()){
-        std::cout << stone.get_angle() << std::endl;
-    }
-    //return problem.field;
     emit answer_ready(problem.field);
 }
