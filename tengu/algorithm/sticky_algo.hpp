f7d58df761b6bab6dc5492d4377622974c98dcd4
imoneg <kazu.takishima@gmail.com>
2015-10-03 13:45:26 +0900

    sticky-algoの書き換え

--- tengu/algorithm/sticky_algo.hpp
+++ tengu/algorithm/sticky_algo.hpp
@@ -1,16 +1,9 @@
 #ifndef STICKY_ALGO_HPP
 #define STICKY_ALGO_HPP
 #include <takao.hpp>
-struct evalated_field{
+class field_with_score_type{
+public:
     field_type field;
-    double score;
-};
-struct putted_evalated_field{
-    int flip;
-    int angle;
-    int y;
-    int x;
-    evalated_field e_field;
     double score;
 };
 
@@ -26,8 +19,8 @@ private:
     const problem_type origin_problem;
     problem_type problem;
     double eval(field_type &field, const stone_type &stone, int pos_y, int pos_x);
-    std::vector<evalated_field> eval_pattern(stone_type stone, std::vector<evalated_field> pattern, int search_width);
-    std::vector<putted_evalated_field> result_stone;
+    std::vector<field_with_score_type> eval_pattern(stone_type stone, std::vector<field_with_score_type> pattern, int search_width);
+    std::vector<field_with_score_type> result_stone;
 };
 
 #endif // STICKY_ALGO_HPP
